852a8426b6572852cd7533e9d45465a5c8eb6742
任鹏飞 <992430303@qq.com>
2018-06-25 17:30:56 +0800

    feat(redux): redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react'
-import logo from './logo.svg'
 import './App.css'
 import {Button} from 'antd-mobile'
-
 class App extends Component {
   render() {
+    const store = this.props.store
+    const removeGun = this.props.removeGun
+    const addGun = this.props.addGun
+    const addGunAsync = this.props.addGunAsync
+    const num = store.getState()
     return (
       <div className="App">
-        <header className="App-header">
-          <Button>default</Button>
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">hello word</h1>
-        </header>
-        <p className="App-intro"></p>
+        <h1>
+          <Button onClick={() => store.dispatch(addGun())}>申请武器</Button>
+          <Button onClick={() => store.dispatch(removeGun())}>上交武器</Button>
+          <Button onClick={() => store.dispatch(addGunAsync())}>延迟上交武器</Button>
+          现在有机枪{num}把
+        </h1>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-
-ReactDOM.render(<App />, document.getElementById('root'))
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import
+{
+    counter,
+    addGun,
+    removeGun,
+    addGunAsync
+}
+from './index.redux.js'
+const store = createStore(counter, compose(applyMiddleware(thunk)))
+// ReactDOM.render(     <App/>, document.getElementById('root'))
+function render() {
+    ReactDOM.render(
+        <App
+        store={store}
+        addGun={addGun}
+        removeGun={removeGun}
+        addGunAsync={addGunAsync}/>, document.getElementById('root'))
+}
+render()
+store.subscribe(render)
 registerServiceWorker()
