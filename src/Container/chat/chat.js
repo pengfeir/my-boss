87aaa48992139ae2f3b190839759bbc452c0c9a4
任鹏飞 <992430303@qq.com>
2018-12-13 16:23:49 +0800

    feat(表情): 表情

--- src/Container/chat/chat.js
+++ src/Container/chat/chat.js
@@ -2,21 +2,22 @@
  * @Author: renpengfei
  * @Date: 2018-11-21 16:22:31
  * @Last Modified by: renpengfei
- * @Last Modified time: 2018-12-12 16:21:30
+ * @Last Modified time: 2018-12-13 16:22:58
  */
 import React from 'react'
 import '../../index.css'
-import { List, InputItem, NavBar,Icon } from 'antd-mobile'
-import { sendMsg ,getMsgList,recvMsg } from '../../redux/chat.redux'
+import { List, InputItem, NavBar, Icon, Grid } from 'antd-mobile'
+import { sendMsg, getMsgList, recvMsg } from '../../redux/chat.redux'
 import { connect } from 'react-redux'
 import { getChatId } from '../../util/util'
-@connect(state => state, { sendMsg ,getMsgList,recvMsg })
+@connect(state => state, { sendMsg, getMsgList, recvMsg })
 class Chat extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             text: '',
-            msg: []
+            // msg: [],
+            showEmoji: false
         }
     }
     handleSubmit() {
@@ -26,59 +27,76 @@ class Chat extends React.Component {
         this
             .props
             .sendMsg(from, to, msg)
-        this.setState({ text: '' })
+        this.setState({ text: '', showEmoji: false })
+    }
+    fixCarepusel() {
+        setTimeout(v => {
+            window.dispatchEvent(new Event('resize'))
+        },0)
     }
     componentWillMount() {
         if (!this.props.chat.chatmsg.length) {
             this
-            .props
-            .getMsgList()
-        this
-            .props
-            .recvMsg()
+                .props
+                .getMsgList()
+            this
+                .props
+                .recvMsg()
         }
-        
+
     }
     render() {
+        const emoji = ("🤣 😃 😄 😅 😆 😉 😊 😋 😎 😍 😘 🥰 😗 😙 😚 🙂 🤗 🤩 🤔 🤨 😐 😑 😶 🙄 😏 😣 �" +
+                "� 😮 🤐 😯 😪 😫 😴 😌 😛 😜 😝 🤤 😒 😓 😔 😕 🙃 🤑 😲 🙁 😖 😞 😟 😤 😢 😭 😦 " +
+                "😧😨 😩 🤯 😬 😰 😱 🥵 🥶 😳 🤪 😵 😡 😠 🤬 😷 🤒 🤕 🤢 🤮 🤧 😇 🤠 🤡 🥳 🥴 🥺 " +
+                "🤥 🤫 🤭 🧐 🤓")
+            .split(' ')
+            .filter(v => v)
+            .map(v => ({ text: v }))
         const userid = this.props.match.params.id
         const Item = List.Item
         const users = this.props.chat.users
         if (!users[userid]) {
             return null
         }
-        const chatid = getChatId(userid,this.props.user._id)
-        const chatmsg = this.props.chat.chatmsg.filter(v => v.chatid === chatid)
+        const chatid = getChatId(userid, this.props.user._id)
+        const chatmsg = this
+            .props
+            .chat
+            .chatmsg
+            .filter(v => v.chatid === chatid)
         return (
             <div id='chat-page'>
-                <NavBar 
-                mode='dark'
-                icon={<Icon type="left" />}
-                onLeftClick={() => this.props.history.goBack()}
-                >
-                 {users[userid].name}
+                <NavBar
+                    mode='dark'
+                    icon={< Icon type = "left" />}
+                    onLeftClick={() => this.props.history.goBack()}>
+                    {users[userid].name}
                 </NavBar>
 
-                {chatmsg
-                    .map((v, index) => {
-                        const avatar = require(`../../Component/img/${users[v.from].avatar}.png`)
-                        return (v.from === userid
-                            ? (
-                                <List key={v._id}>
-                                    <Item 
-                                     thumb = {avatar}
-                                    >
-                                        {v.content}
-                                    </Item>
-                                </List>
-                            )
-                            : (
-                                <List key={v._id}>
-                                    <Item className='chat-me' extra ={<img src={avatar}/>}>
-                                        {v.content}
-                                    </Item>
-                                </List>
-                            ))
-                    })}
+                {chatmsg.map((v, index) => {
+                    const avatar = require(`../../Component/img/${users[v.from].avatar}.png`)
+                    return (v.from === userid
+                        ? (
+                            <List key={v._id}>
+                                <Item thumb={avatar}>
+                                    {v.content}
+                                </Item>
+                            </List>
+                        )
+                        : (
+                            <List key={v._id}>
+                                <Item
+                                    className='chat-me'
+                                    extra
+                                    ={< img src = {
+                                    avatar
+                                } />}>
+                                    {v.content}
+                                </Item>
+                            </List>
+                        ))
+                })}
                 <p></p>
                 <div className="stick-footer">
                     <List>
@@ -88,12 +106,28 @@ class Chat extends React.Component {
                             onChange={v => {
                             this.setState({ text: v })
                         }}
-                            extra={
-                            <span onClick = {() => this.handleSubmit()} >发送
-                            </span>}
-                            >
-                        </InputItem>
+                            extra={<div><span
+                            style={{
+                            marginRight: 15
+                        }}
+                            onClick=
+                            {
+                                () => {
+this.setState({ showEmoji: !this.state.showEmoji })
+                            this.fixCarepusel()
+}
+                            }>😃
+                        </span><span onClick = {
+                            () => this.handleSubmit()
+                        } > 发送 </span> </div>}></InputItem>
                     </List>
+                    {this.state.showEmoji
+                        ? <Grid data={emoji} columnNum={9} carouselMaxRow={4} isCarousel={true}
+                        onClick={el => {
+this.setState({ text: this.state.text + el.text })
+                        }}
+                        />
+                        : null}
                 </div>
             </div>
         )
